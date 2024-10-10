3e5a472f72b7bd026e18da626e71ca237d98d744
CristoMaster <cristopher.corrales@udea.edu.co>
2024-10-10 00:11:48 -0500

    Clase TanqueCombustible
    Se crea la clase TanqueCombustible con sus respectivos metodos.
    
    Se le asginan valores aleatorios entre 100 y 200 al tipo de gasolina, se crean surtidores entre 2 y 12 y a cada surtidor se le asigna un modelo

--- DesafioII/clases.h
+++ DesafioII/clases.h
@@ -1,10 +1,32 @@
 #ifndef CLASES_H
 #define CLASES_H
+#include <string>
+using namespace std;
 
-class Clases
-{
+class redEstaciones;
+class TanqueCombustible {
+private:
+    float regular, premium, ecoExtra;
+    unsigned char surtidores, naves;
+    string* modeloSurtidor;
+
 public:
-    Clases();
-};
+    //constructor
+    TanqueCombustible();
 
+    // Métodos para obtener la cantidad de combustible y vender
+    // Getter
+    float obtenerCantidadCombustible(const string& tipo) const;
+    unsigned char obtenerSurtidores() const;
+    unsigned char obtenerNaves()const;
+
+    //metodo
+    void ventaCombustible(const string& tipo, float cantidad);
+    void modificarSurtidores();
+
+    unsigned char obtenerNaves();
+    void asignarModelosSurtidores();
+
+
+};
 #endif // CLASES_H
